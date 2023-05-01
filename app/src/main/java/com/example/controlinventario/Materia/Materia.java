4a2fd25c3390c3b60ea339b6c65d637293e18ba9
Diego Mejia <pcdiego17@gmail.com>
2023-05-01 15:51:32 -0600

    add package and activity for autor,libro,materia

---
+++ app/src/main/java/com/example/controlinventario/Materia/Materia.java
@@ -0,0 +1,16 @@
+package com.example.controlinventario.Materia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.controlinventario.R;
+
+public class Materia extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_materia);
+    }
+}
