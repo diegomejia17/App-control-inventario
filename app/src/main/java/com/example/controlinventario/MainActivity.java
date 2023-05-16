db7cf0c5a9b6a0a6a9c2cae377cf635e85e8cad2
Diego Mejia <pcdiego17@gmail.com>
2023-05-16 10:05:45 -0600

    agregando menu

--- app/src/main/java/com/example/controlinventario/MainActivity.java
+++ app/src/main/java/com/example/controlinventario/MainActivity.java
@@ -1,14 +1,47 @@
 package com.example.controlinventario;
 
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+    String values[] = {
+            "Autor.AutorActivity", "Libro.LibroActivity",
+            "Materia.MateriaActivity", "CheckBoxActivity",
+            "RadioButtonActivity", "GalleryActivity",
+            "SpinnerActivity", "TabWidgetActivity"
+    };
+    String ViewValues[]={
+        "Autor","Libro","Materia"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayAdapter<String> adaptador =new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,ViewValues);
+        ListView listView =(ListView)findViewById(R.id.listV);
+        listView.setAdapter(adaptador);
+        listView.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        String nameValue = values[i];
+        try {
+            Class<?> clase = Class.forName("com.example.controlinventario." + nameValue);
+            startActivity(new Intent(this, clase));
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
     }
 }
